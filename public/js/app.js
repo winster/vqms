1834bda9c451b0af2f125060062c602af257283e
wjose <winster.jose@amadeus.com>
2016-05-10 16:50:59 +0530

    modified for facebook login

--- public/js/app.js
+++ public/js/app.js
@@ -9,15 +9,6 @@ function statusChangeCallback(response) {
   if (response.status === 'connected') {
     // Logged into your app and Facebook.
     testAPI();
-  } else if (response.status === 'not_authorized') {
-    // The person is logged into Facebook, but not your app.
-    document.getElementById('status').innerHTML = 'Please log ' +
-      'into this app.';
-  } else {
-    // The person is not logged into Facebook, so we're not sure if
-    // they are logged into this app or not.
-    document.getElementById('status').innerHTML = 'Please log ' +
-      'into Facebook.';
   }
 }
 
@@ -34,7 +25,7 @@ function checkLoginState() {
 // Here we run a very simple test of the Graph API after login is
 // successful.  See statusChangeCallback() for when this call is made.
 function testAPI() {
-  console.log('Welcome!  Fetching your information.... ');
+  console.log('Welcome!  Fetching your information.... '+isPushEnabled);
   FB.api('/me', function(response) {
     console.log('Successful login for: ' + response.name);
     
